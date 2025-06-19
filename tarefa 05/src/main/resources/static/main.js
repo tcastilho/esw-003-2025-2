70ce6a02366ffebc85dee3834e782d6c24bc4f48
giordano26 <smfgiordano@gmail.com>
2025-06-19 01:19:35 -0300

    html, css e js para tarefa 5

--- tarefa 05/src/main/resources/static/main.js
+++ tarefa 05/src/main/resources/static/main.js
@@ -1,10 +1,34 @@
 import { loadPacotes } from "./stub.js";
 
 function onLoadPacotes() {
-    const placeholder = document.getElementById("placeholder");
-    placeholder.innerHTML = "<p>Loading</p>"
+    const table = document.getElementById("table");
+    table.innerHTML = `
+        <tr>
+            <th>Descrição</th>
+            <th>Cidade</th>
+            <th>Nome do Hotel</th>
+            <th>Preço</th>
+        </tr>
+    `
+
     loadPacotes((data) => {
-        placeholder.innerHTML=`<p>${data.length} pacotes carregados</p>`;
+        for(let i = 0; i < data.length; i++){
+
+             let precoPacote = 0.0;
+             for(let j = 0; j < data[i].items.length; j++){
+                precoPacote += data[i].items[j].preco;
+             }
+
+            table.innerHTML += `
+            <tr>
+                <td>${data[i].descricao}</td>
+                <td>${data[i].localidade.descricao}</td>
+                <td>${data[i].items[0].nomeHotel}</td>
+                <td>R$ ${precoPacote.toFixed(2)}</td>
+            </tr>
+            `
+        }
+
     })
 }
 
